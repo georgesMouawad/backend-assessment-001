7683c276ccd106ba74c10130c12aa7cd520ebbe0
George <g.m.6@live.com>
2024-06-04 19:00:09 +0300

    Api claimservice implementation

--- api/src/services/claim/claim.service.ts
+++ api/src/services/claim/claim.service.ts
@@ -6,10 +6,17 @@ import { Subdomain } from 'src/interfaces/subdomain.interface';
 
 @Injectable()
 export class ClaimService implements OnModuleInit {
-    justAName: JustaName
+    justAName: JustaName;
+    chainId: number;
+    domain: string;
+    origin: string;
+    ensDomain: string;
 
     constructor(readonly configService: ConfigService) {
-
+        this.chainId = parseInt(this.configService.get('JUSTANAME_CHAIN_ID'));
+        this.domain = this.configService.get('JUSTANAME_DOMAIN');
+        this.origin = this.configService.get('JUSTANAME_ORIGIN');
+        this.ensDomain = this.configService.get('JUSTANAME_ENS_DOMAIN');
     }
 
     async onModuleInit(): Promise<void> {
@@ -22,7 +29,35 @@ export class ClaimService implements OnModuleInit {
         });
     }
 
-    async claimSubdomain(request: Subdomain)
+    async addSubdomain(request: Subdomain): Promise<any> {
+        if (!request.username) {
+            return {
+                message: 'Username is required',
+            }
+        }
+
+        try {
+
+            const addResponse = await this.justAName.subnames.addSubname({
+                username: request.username,
+                ensDomain: this.ensDomain,
+                chainId: this.chainId,
+            }, {
+                xSignature: request.signature,
+                xAddress: request.address,
+                xMessage: request.message,
+            })
+
+            return addResponse;
+
+        } catch (error) {
+
+            return {
+                error: error.message
+            };
+
+        }
+    }
 
 
 
