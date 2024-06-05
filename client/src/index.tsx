3cdbc814b36df7b8d7c49c9b9b9c9f42eef3a0b1
George <g.m.6@live.com>
2024-06-05 11:55:05 +0300

    Client homepage implementation

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { JustaNameProvider } from '@justaname.id/react';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+require('dotenv').config();
+const chainId = process.env.REACT_APP_CHAIN_ID ? parseInt(process.env.REACT_APP_CHAIN_ID) : undefined;
+const validChainId = chainId === 1 || chainId === 11155111 ? chainId : undefined;
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <JustaNameProvider backendUrl={process.env.REACT_APP_API_URL} chainId={validChainId}>
+                <App />
+            </JustaNameProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
