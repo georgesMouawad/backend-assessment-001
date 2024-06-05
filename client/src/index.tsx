6c0993d959067f6add6bdded164fbd3421a8335d
George <g.m.6@live.com>
2024-06-05 12:47:59 +0300

    Client homepage implementation

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { JustaNameProvider } from '@justaname.id/react';
+// import { JustaNameProvider } from '@justaname.id/react';
 
-require('dotenv').config();
-const chainId = process.env.REACT_APP_CHAIN_ID ? parseInt(process.env.REACT_APP_CHAIN_ID) : undefined;
-const validChainId = chainId === 1 || chainId === 11155111 ? chainId : undefined;
+// require('dotenv').config();
+// const chainId = process.env.REACT_APP_CHAIN_ID ? parseInt(process.env.REACT_APP_CHAIN_ID) : undefined;
+// const validChainId = chainId === 1 || chainId === 11155111 ? chainId : undefined;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <JustaNameProvider backendUrl={process.env.REACT_APP_API_URL} chainId={validChainId}>
+            {/* <JustaNameProvider backendUrl={process.env.REACT_APP_API_URL} chainId={validChainId}> */}
                 <App />
-            </JustaNameProvider>
+            {/* </JustaNameProvider> */}
         </BrowserRouter>
     </React.StrictMode>
 );
